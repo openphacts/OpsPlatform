c5f38a83a812653a74f5362113a7661c6fbc7d9a
Christian-B <brenninc@cs.man.ac.uk>
2011-12-02 12:21:02 +0000

    Paul: modified to return to directory from where the script was ran (svn 18)

--- ops-platform/scripts/RunLarKC.sh
+++ ops-platform/scripts/RunLarKC.sh
@@ -1,3 +1,6 @@
 # Launch LarKC
+currentdir=`pwd`
 cd $LARKC_PATH/larkc/trunk/platform/
 java -cp plugins/plugin.QueryMapper-0.0.1-SNAPSHOT-LarkcPluginAssembly/lib/derby.jar:./target/platform-2.0.0-SNAPSHOT-jar-with-dependencies.jar -Xmx1G eu.larkc.core.Larkc
+
+cd $currentdir
