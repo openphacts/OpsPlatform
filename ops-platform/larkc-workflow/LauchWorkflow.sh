e54541f3641f4f7dc56f61a219c9572d6bf78c9e
Christian-B <brenninc@cs.man.ac.uk>
2011-12-02 12:44:04 +0000

    paul: removed scripts (svn 29)

--- ops-platform/larkc-workflow/LauchWorkflow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-WORKFLOW=`cat simpleworkflow.ttl`
-ENDPOINT_ID="urn:eu.larkc.endpoint.sparql.ep1"
-WID=`curl  --data "workflow=$WORKFLOW" http://localhost:8182/rdf/workflows`
-curl "http://localhost:8182/rdf/workflows/$WID/endpoint?urn=$ENDPOINT_ID"
-echo
