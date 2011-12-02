ba125f4f033d99b57d9884381d11ad22d0fe4fd0
Christian-B <brenninc@cs.man.ac.uk>
2011-12-02 12:41:39 +0000

    kot: updated scripts (svn 27)

--- ops-platform/scripts/BuildLarKC.sh
+++ ops-platform/scripts/BuildLarKC.sh
@@ -9,15 +9,15 @@ cd $LARKC_PATH/larkc/trunk/platform/
 mvn assembly:assembly -DdescriptorId=jar-with-dependencies -Dmaven.test.skip=true
 
 cd ../plugins/NewFileIdentifier
-mvn assembly:assembly
+mvn install
 mv ./target/*SNAPSHOT.jar   ../../platform/plugins
 
 cd ../NewSparqlQueryEvaluationReasoner
-mvn assembly:assembly
+mvn install
 mv ./target/*SNAPSHOT.jar   ../../platform/plugins
 
 cd ../RDFReader
-mvn assembly:assembly
+mvn install
 mv ./target/*SNAPSHOT.jar   ../../platform/plugins
 cd $OPS_PATH
 cd openphacts/ops-platform/larkc-plugins/
@@ -25,13 +25,13 @@ cd plugin.querymapper/
 mvn assembly:assembly
 mv ./target/plugin.QueryMapper-0.0.1-SNAPSHOT-LarkcPluginAssembly.jar $LARKC_PATH/larkc/trunk/platform/plugins
 cd ../plugin.edffilter/
-mvn assembly:assembly
+mvn install
 mv ./target/plugin.EDFFilter-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/trunk/platform/plugins
 cd ../plugin.edfquerytransformer/
-mvn assembly:assembly
+mvn install
 mv ./target/plugin.EDFQueryTransformer-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/trunk/platform/plugins
 cd ../plugin.edfsearch/
-mvn assembly:assembly
+mvn install
 mv ./target/plugin.EDFSearch-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/trunk/platform/plugins
 
 cd $currentdir
