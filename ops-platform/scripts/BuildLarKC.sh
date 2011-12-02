68768b1344a01bedc6048a9232494a3596ea66ee
Christian-B <brenninc@cs.man.ac.uk>
2011-12-02 12:51:49 +0000

    Paul: updates to support openphacts branch (svn 39)

--- ops-platform/scripts/BuildLarKC.sh
+++ ops-platform/scripts/BuildLarKC.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export MAVEN_OPTS=-Xmx512m
-# LARKC_PATH=/tmp/cleanrun # path where LarKC is checked out: svn co https://larkc.svn.sourceforge.net/svnroot/larkc/trunk larkc/trunk
+# LARKC_PATH=/tmp/cleanrun # path where LarKC is checked out: svn co https://larkc.svn.sourceforge.net/svnroot/larkc/branches/openphacts larkc/branches/openphacts
 # OPS_PATH=/tmp/cleanrun # - path where OPS repository is checked out: svn co  https://trac.nbic.nl/svn/openphacts
 
 currentdir=`pwd`
 
-cd $LARKC_PATH/larkc/trunk/platform/
+cd $LARKC_PATH/larkc/branches/openphacts/platform/
 mvn assembly:assembly -DdescriptorId=jar-with-dependencies -Dmaven.test.skip=true
 
 cd ../plugins/NewFileIdentifier
@@ -24,18 +24,18 @@ cd $OPS_PATH
 cd openphacts/ops-platform/larkc-plugins/
 cd plugin.querymapper/
 mvn assembly:assembly
-mv ./target/plugin.QueryMapper-0.0.1-SNAPSHOT-LarkcPluginAssembly.jar $LARKC_PATH/larkc/trunk/platform/plugins
+mv ./target/plugin.QueryMapper-0.0.1-SNAPSHOT-LarkcPluginAssembly.jar $LARKC_PATH/larkc/branches/openphacts/platform/plugins
 
 cd ../plugin.edffilter/
 mvn install
-mv ./target/plugin.EDFFilter-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/trunk/platform/plugins
+mv ./target/plugin.EDFFilter-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/branches/openphacts/platform/plugins
 cd ../plugin.edfquerytransformer/
 
 mvn install
-mv ./target/plugin.EDFQueryTransformer-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/trunk/platform/plugins
+mv ./target/plugin.EDFQueryTransformer-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/branches/openphacts/platform/plugins
 cd ../plugin.edfsearch/
 
 mvn install
-mv ./target/plugin.EDFSearch-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/trunk/platform/plugins
+mv ./target/plugin.EDFSearch-0.0.1-SNAPSHOT.jar $LARKC_PATH/larkc/branches/openphacts/platform/plugins
 
 cd $currentdir
--- ops-platform/scripts/RunLarKC.sh
+++ ops-platform/scripts/RunLarKC.sh
@@ -1,7 +1,7 @@
 #LARKC_PATH #- path where LarKC is checked out: svn co https://larkc.svn.sourceforge.net/svnroot/larkc/trunk larkc/trunk
 # Launch LarKC
 currentdir=`pwd`
-cd $LARKC_PATH/larkc/trunk/platform/
+cd $LARKC_PATH/larkc/branches/openphacts/platform/
 rm -rf ordi-trree
 java -cp plugins/plugin.QueryMapper-0.0.1-SNAPSHOT-LarkcPluginAssembly/lib/derby.jar:./target/platform-2.5.0-SNAPSHOT-jar-with-dependencies.jar -Xmx1G eu.larkc.core.Larkc
 
