3d897583a62fcbe2f1e77223829227d84bf1292b
Christian-B <brenninc@cs.man.ac.uk>
2011-12-04 19:39:20 +0000

    a.loizou: Changed LaunchOPSAPIWorkflow.sh script for LarKC 3.0 (svn 162-163)

--- ops-platform/scripts/LaunchOPSAPIWorkflow.sh
+++ ops-platform/scripts/LaunchOPSAPIWorkflow.sh
@@ -2,7 +2,7 @@
 # OPS_PATH= # - path where OPS repository is checked out: svn co  https://trac.nbic.nl/svn/openphacts
 #!/bin/bash
 WORKFLOW=`cat $OPS_PATH/openphacts/ops-platform/larkc-workflow/OPS_API_workflow.ttl`
-ENDPOINT_ID="urn:eu.larkc.endpoint.opsapi.ep1"
-WID=`curl  --data "workflow=$WORKFLOW" http://localhost:8182/rdf/workflows`
-curl "http://localhost:8182/rdf/workflows/$WID/endpoint?urn=$ENDPOINT_ID"
-echo
+HTML=`curl  --data "workflow=$WORKFLOW" http://localhost:8182/rdf/workflows`
+WID=`echo $HTML | head -n1 |sed 's/[[:print:]]*<b>\([[:print:]]*\)<\/b> succ[[:print:]]*/\1/'`
+echo "WID: "$WID
+echo $HTML | sed "s/[[:print:]]*$WID<\/td><td>ep (\([.:\/a-z0-9]*\)[[:print:]]*/Endpoint URL: \1/"
