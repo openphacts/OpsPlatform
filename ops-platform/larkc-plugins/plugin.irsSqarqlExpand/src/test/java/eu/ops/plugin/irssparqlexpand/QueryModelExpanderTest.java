fb71c3d46bcb1d4e49d64ce98904720996e43fb3
Alasdair Gray <A.Gray@cs.man.ac.uk>
2011-12-16 12:46:53 +0000

    First version of the query expander operating on parse tree. Capable of expanding a single BGP query with a URI in the object

---
+++ ops-platform/larkc-plugins/plugin.irsSqarqlExpand/src/test/java/eu/ops/plugin/irssparqlexpand/QueryModelExpanderTest.java
@@ -0,0 +1,124 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.ops.plugin.irssparqlexpand;
+
+import eu.larkc.core.query.SPARQLQueryImpl;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import static org.easymock.EasyMock.expect;
+import org.easymock.EasyMockSupport;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.parser.ParsedQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
+public class QueryModelExpanderTest extends EasyMockSupport {
+
+    private static Logger logger = LoggerFactory.getLogger(QueryModelExpanderTest.class);
+    
+    public QueryModelExpanderTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of meet method, of class QueryModelExpander.
+     * 
+     * Test a query involving a BGP with an object URI and an existing
+     * FILTER clause.
+     */
+    @Test
+    public void testMeet_oneBgpObjectUriMultipleMatches() 
+            throws QueryModelExpanderException, UnexpectedQueryException, MalformedQueryException {
+        final Map<URI, List<URI>> mockMap = createMock(Map.class);
+        final List<URI> mockList = createMock(List.class);
+        final Iterator<URI> mockIterator = createMock(Iterator.class);
+        expect(mockMap.get(new URIImpl("http://brenda-enzymes.info/1.1.1.1")))
+                .andReturn(mockList);
+        expect(mockList.size()).andReturn(2);
+        expect(mockList.add(new URIImpl("http://brenda-enzymes.info/1.1.1.1")))
+                .andReturn(Boolean.TRUE);
+        expect(mockList.iterator()).andReturn(mockIterator);
+        expect(mockIterator.hasNext())
+                .andReturn(Boolean.TRUE).times(2).andReturn(Boolean.FALSE);
+        expect(mockIterator.next())
+                .andReturn(new URIImpl("http://example.com/983juy"))
+                .andReturn(new URIImpl("http://brenda-enzymes.info/1.1.1.1"));
+        replayAll();
+        String expectedQuery = "SELECT ?protein "
+                + "WHERE {"
+                + "?protein <http://www.biopax.org/release/biopax-level2.owl#EC-NUMBER> "
+                + "?objectUri1 . "
+                + "FILTER (?objectUri1 = <http://example.com/983juy> || "
+                + "?objectUri1 = <http://brenda-enzymes.info/1.1.1.1>) . "
+                + "}";
+        String query = "SELECT ?protein "
+                + "WHERE {"
+                + "?protein <http://www.biopax.org/release/biopax-level2.owl#EC-NUMBER> "
+                + "<http://brenda-enzymes.info/1.1.1.1> . "
+                + "}";
+        final ParsedQuery parsedQuery = new SPARQLQueryImpl(query).getParsedQuery();
+        final TupleExpr tupleExpr = parsedQuery.getTupleExpr();
+        QueryModelExpander qme = new QueryModelExpander(mockMap);
+        tupleExpr.visit(qme);
+System.out.println("**Tuple Expr:\n" + tupleExpr);
+        String expandedQuery = QueryUtils.tupleExprToQueryString(tupleExpr);
+System.out.println("**Expanded query:\n" + expandedQuery);        
+        assertTrue(QueryUtils.sameTupleExpr(expectedQuery, expandedQuery));
+//        assertEquals(qme, mockMap);
+    }
+
+    /**
+     * Test of meet method, of class QueryModelExpander.
+     * 
+     * Test a query involving a BGP with an object URI and an existing
+     * FILTER clause
+     */
+    @Test@Ignore
+    public void testMeet_oneBgpObjectUriWithFilter() throws QueryModelExpanderException {
+        final Map<URI, List<URI>>mockMap = createMock(Map.class);
+        replayAll();
+        String query = "SELECT ?protein "
+                + "WHERE {"
+                + "?protein <http://www.biopax.org/release/biopax-level2.owl#EC-NUMBER> "
+                + "<http://brenda-enzymes.info/1.1.1.1> . "
+                + "FILTER (?protein = <http://something.org>) . "
+                + "}";
+        final ParsedQuery parsedQuery = new SPARQLQueryImpl(query).getParsedQuery();
+        final TupleExpr tupleExpr = parsedQuery.getTupleExpr();
+        QueryModelExpander qme = new QueryModelExpander(mockMap);
+        tupleExpr.visit(qme);
+        
+    }
+    
+}
