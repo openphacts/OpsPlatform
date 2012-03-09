142212034fb60f70c979c6103c48e32accd4697c
antonisloizou <a.loizou@vu.nl>
2012-03-09 15:08:40 +0100

    Updated Stress tester

--- OPSWorkflowUtils/src/main/java/eu/ops/utils/ClientThread.java
+++ OPSWorkflowUtils/src/main/java/eu/ops/utils/ClientThread.java
@@ -30,25 +30,52 @@ public class ClientThread implements Runnable {
                 + "{ ?s ?p ?o }"
                 + "Limit 10";
     //private static String endpoint = "http://localhost:8183/opsapi";
-    private String endpoint = "http://ops.few.vu.nl:8183/opsapi"; //production
+    private String endpoint = "http://ops.few.vu.nl:9189/opsapi"; //production
 
     private UrlEncodedFormEntity entity;
+    
+    private String myMethod;
+    
+    private String bogusURI = "<http://example.com/protein123>";
     
     private int threadNumber;
     
     public ClientThread(int number) throws UnsupportedEncodingException{
         List<NameValuePair> formparams = new ArrayList<NameValuePair>();
 		formparams.add(new BasicNameValuePair("query", query));
-		formparams.add(new BasicNameValuePair("method", "sparql"));        
-		entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+		formparams.add(new BasicNameValuePair("method", "sparql"));
         threadNumber = number;
+    }
+    
+    public ClientThread(int number, int method){
+    	if (method==0 || method==6)
+    		myMethod="compoundInfo";
+    	else if (method==1)
+    		myMethod="compoundPharmacology";
+    	else if (method==2)
+    		myMethod="compoundInfo";
+    	else if (method==3)
+    		myMethod="proteinPharmacology";
+    	else if (method==4)
+    		myMethod="proteinInfo";
+    	else if (method==5)
+    		myMethod="enzymeClassPharmacology";
+    	threadNumber=number;    	
     }
     
     @Override
     public void run() {
         List<NameValuePair> formparams = new ArrayList<NameValuePair>();
-		formparams.add(new BasicNameValuePair("query", query));
-		formparams.add(new BasicNameValuePair("method", "sparql"));
+		if (myMethod=="enzymeClassPharmacology")
+			formparams.add(new BasicNameValuePair("class", "1.1.1.-"));
+		else
+        	formparams.add(new BasicNameValuePair("uri", bogusURI));
+		formparams.add(new BasicNameValuePair("method", myMethod));        
+		try {
+			entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 		HttpPost httppost = new HttpPost(endpoint);
 		httppost.setEntity(entity);
         Date start = new Date();
@@ -58,14 +85,20 @@ public class ClientThread implements Runnable {
                 HttpResponse response = httpclient.execute(httppost);
                 HttpEntity responseEntity = response.getEntity();
                 if (responseEntity != null) {
-                    System.out.println(EntityUtils.toString(responseEntity));
+                    //System.out.println(EntityUtils.toString(responseEntity));
                 }
 
             } catch (IOException ex) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println(threadNumber + ": " + i);
+            /*try {
+				//Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
+            System.out.println("Thread" + threadNumber + ": " + i);
         }
         Date end = new Date();
         System.out.println ("done " + threadNumber);
--- OPSWorkflowUtils/src/main/java/eu/ops/utils/EndPointTester.java
+++ OPSWorkflowUtils/src/main/java/eu/ops/utils/EndPointTester.java
@@ -15,15 +15,17 @@ import org.apache.http.ParseException;
  */
 public class EndPointTester {
     
-    private static final int NUMBER_OF_THREADS = 10;
+    private static final int NUMBER_OF_THREADS = 20;
     
     public static void main(String[] args) throws ParseException, IOException {
         Thread[] threads = new Thread[NUMBER_OF_THREADS];
         for (int i = 0; i < NUMBER_OF_THREADS ; i++){
-            threads[i] = new Thread(new ClientThread(i));
+        	int method=new Double(Math.round(Math.random()*6)).intValue();
+            threads[i] = new Thread(new ClientThread(i,method));
             System.out.println(i);
         }
         for (int i = 0; i < NUMBER_OF_THREADS ; i++){
+        	System.out.println("Starting thread: "+ i);
             threads[i].start();
         }        
     }
